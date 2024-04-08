e3d83603951e64f44a6c320c909115e63aba1188
VolskiyRoman <romanvolskiy791@gmail.com>
2024-04-08 22:27:41 +0300

    hide secret :)

--- my_app/settings.py
+++ my_app/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zg**6zgl49y3^&_!pd4z*@kpw(b(e%l6rz2v_l=s9l+4a-&*7t'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
